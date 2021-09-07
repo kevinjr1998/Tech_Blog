c120b4f0df6ef633937baab2182eb36728c45efa
Kevin98 <kevinryner@yahoo.co.uk>
2021-09-07 21:30:42 +0100

    application will render data onto page now

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,23 @@
 
 const router = require('express').Router();
-
+const { Users, Posts } = require("../models");
 
 // "/" route handlers
 
-router.get('/', function (req, res) {
-    res.render('home');
+router.get('/', async function (req, res) {
+    const postData = await Posts.findAll({
+        include: [
+            {
+              model: Users,
+              attributes: ['name'],
+            },
+          ],
+    });
+    const blogPosts = postData.map((eachPost) => 
+    eachPost.get({ plain: true })
+    );
+    debugger;
+    res.render('home', { blogPosts });
 });
 
 
